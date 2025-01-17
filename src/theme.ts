2253f0f65157c5dd920b02dc6d58089582c915af
Rami <138556019+rami242424@users.noreply.github.com>
2025-01-17 19:47:44 +0900

    dark mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
-import { DefaultTheme } from "styled-components" ;
+import { DefaultTheme } from "styled-components";
 
-export const theme : DefaultTheme = {
-    // bgColor : "white",
-    // textColor : "black",
-    btnColor : "tomato",
+export const theme: DefaultTheme = {
+    bgColor: "white",
+    textColor: "black",
+    btnColor: "tomato",
 }

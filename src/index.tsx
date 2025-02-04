2aa75bcb2811493907a73ef9888b01f6518b5154
Rami <138556019+rami242424@users.noreply.github.com>
2025-02-04 20:44:27 +0900

    3.6 Theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
@@ -8,7 +10,9 @@ root.render(
 
 
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   
 );

693be2257267406079169de7d4b64224ee506acf
Rami <138556019+rami242424@users.noreply.github.com>
2025-02-05 21:22:39 +0900

    #5.2 Home part One

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,10 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
   
 );

622ffcdb6058b1e2b2af6e5ed770f46e246fae6a
Rami <138556019+rami242424@users.noreply.github.com>
2025-02-07 20:07:27 +0900

    #5.4 Route States

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -24,9 +24,11 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom: 10px;
     a {
+        display: flex;
+        align-items: center;
+
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
     &:hover {
         a {
@@ -43,6 +45,12 @@ const Title = styled.h1`
 const Loader = styled.span`
     text-align : center;
     display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -76,7 +84,11 @@ function Coins(){
             {loading ? <Loader>Loading...</Loader> : <CoinsList>
                 {coins.map((coin) => (
                 <Coin key={coin.id}>
-                    <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                    <Link to={`/${coin.id}`}>
+                        <Img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLowerCase()}`}
+                        />
+                        {coin.name} &rarr;
+                    </Link>
                 </Coin>
                 ))}
             </CoinsList>}

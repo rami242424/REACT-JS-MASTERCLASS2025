b53b1ce9118354a6d0296ef6b7a6f3143a3be5fc
Rami <138556019+rami242424@users.noreply.github.com>
2025-02-18 23:13:28 +0900

    5.12 price chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,13 @@
-function Chart() {
-    return <h1>This is coin Chart</h1>;
-  }
-  export default Chart;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return <h1>Chart</h1>;
+}
+
+export default Chart;

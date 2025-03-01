388b1f875568bb648821e7cff0ff80d56ef9add6
Rami <138556019+rami242424@users.noreply.github.com>
2025-03-01 20:26:37 +0900

    6.0 dark mode index

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-root.render(
-  <>
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </ThemeProvider>
-  </QueryClientProvider>
-  </>,
+
+
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );

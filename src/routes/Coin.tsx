00e1a6f5c2b07772319393d037066cddf09c9ffb
Rami <138556019+rami242424@users.noreply.github.com>
2025-03-19 23:28:05 +0900

    5.12 price chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,4 @@
 import { useQuery } from "react-query";
-import { Helmet } from "react-helmet";
 import {
   Switch,
   Route,
@@ -152,19 +151,11 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId),
-    {
-      refetchInterval: 5000,
-    }
+    () => fetchCoinTickers(coinId)
   );
   const loading = infoLoading || tickersLoading;
   return (
     <Container>
-      <Helmet>
-        <title>
-          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
-        </title>
-      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -184,8 +175,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Price:</span>
-              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+              <span>Open Source:</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -67,9 +66,6 @@ function Coins() {
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
-      <Helmet>
-        <title>코인</title>
-      </Helmet>
       <Header>
         <Title>코인</Title>
       </Header>

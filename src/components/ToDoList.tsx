e760218ede69a1a83c4bf4bc77520fda75cfed0e
Rami <138556019+rami242424@users.noreply.github.com>
2025-03-27 22:44:17 +0900

    6.14 Immutability part one

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -3,42 +3,14 @@ import { toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
-
-
-
-
-
-
-
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
-
-
-
-
-
-
-
-
       <ul>
         {toDos.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
